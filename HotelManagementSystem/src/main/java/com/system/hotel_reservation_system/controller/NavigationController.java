f05a3c3bb38ff83f56285294a85add3e3261a8f1
Utsavraut <raututsav35@gmail.com>
2023-01-19 08:19:11 +0545

    Changes

---
+++ HotelManagementSystem/src/main/java/com/system/hotel_reservation_system/controller/NavigationController.java
@@ -0,0 +1,22 @@
+package com.system.hotel_reservation_system.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping
+public class NavigationController {
+    @GetMapping("/about")
+    public String GetAbout(){
+        return "/about";
+    }
+
+    @GetMapping("/gallery")
+    public String GetGallery(){
+        return "/gallery";
+    }
+
+}
