eed36a1e6127711afc246d57aa3405062d17ff37
Utsavraut <raututsav35@gmail.com>
2023-01-27 16:13:22 +0545

    Changes

--- HotelManagementSystem/src/main/java/com/system/hotel_reservation_system/controller/BookingController.java
+++ HotelManagementSystem/src/main/java/com/system/hotel_reservation_system/controller/BookingController.java
@@ -1,4 +1,25 @@
 //package com.system.hotel_reservation_system.controller;
 //
+//
+//import com.system.hotel_reservation_system.pojo.BookPojo;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import java.security.Principal;
+//import java.text.ParseException;
+//
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping
 //public class BookingController {
+//    private final BookingService bookingService;
+//
+//    @PostMapping("/book")
+//    public String bookBike(@Valid BookPojo bookingPojo) throws ParseException {
+//        bookingService.saveBooking(bookingPojo);
+//        return "redirect:/home";
+//    }
 //}
