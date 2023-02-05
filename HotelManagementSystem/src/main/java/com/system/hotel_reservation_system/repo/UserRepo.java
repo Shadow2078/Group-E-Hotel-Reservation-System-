b10b71523415a8f5c5292df3da0fdb0d39b0a68d
anilaghimire <amiableella6@gmail.com>
2023-02-05 08:50:34 +0545

    about page started

--- HotelManagementSystem/src/main/java/com/system/hotel_reservation_system/repo/UserRepo.java
+++ HotelManagementSystem/src/main/java/com/system/hotel_reservation_system/repo/UserRepo.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
-<<<<<<< HEAD
+//<<<<<<< HEAD
     Optional<Object> findByPassword(String password);
 
     Optional<Object> findByEmail(String email);
@@ -17,8 +17,8 @@ public interface UserRepo extends JpaRepository<User, Integer> {
 
     @Query(value = "UPDATE users SET password =?1 WHERE email = ?2", nativeQuery = true)
     void updatePassword(String password, String email);
-=======
+//=======
     @Query(value = "select * from USERS where email=?1", nativeQuery = true)
     Optional<User> findByEmail(String email);
->>>>>>> 17123dc8ac4276e53f0c0efd260a6fe44cc48168
+//>>>>>>> 17123dc8ac4276e53f0c0efd260a6fe44cc48168
 }
